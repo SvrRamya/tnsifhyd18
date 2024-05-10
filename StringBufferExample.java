16f4ed49f7078205280052c34295e48ab0b51bb9
SvrRamya <168424860+SvrRamya@users.noreply.github.com>
2024-05-10 23:43:52 +0530

    Add files via upload

---
+++ StringBufferExample.java
@@ -0,0 +1,13 @@
+//1) StringBuffer Class append() Method
+
+
+package com.si.package1;
+
+public class StringBufferExample {
+	public static void main(String args[]){  
+		StringBuffer sb=new StringBuffer("Hello ");  
+		sb.append("Java");//now original string is changed  
+		System.out.println(sb);//prints Hello Java  
+		}  
+
+}
