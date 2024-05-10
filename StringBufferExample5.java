848bab66bf22b461fa84deb886ff6b444384e192
SvrRamya <168424860+SvrRamya@users.noreply.github.com>
2024-05-10 23:45:44 +0530

    Add files via upload

---
+++ StringBufferExample5.java
@@ -0,0 +1,14 @@
+//5) StringBuffer reverse() Method
+
+
+package com.si.package1;
+
+public class StringBufferExample5 {
+	public static void main(String args[]){  
+		StringBuffer sb=new StringBuffer("Hello");  
+		sb.reverse();  
+		System.out.println(sb);//prints olleH  
+		}  
+
+
+}
