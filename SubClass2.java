a24325cde90895f8eaf1d9721324f9e76ede3f12
SvrRamya <168424860+SvrRamya@users.noreply.github.com>
2024-05-11 00:23:22 +0530

    Add files via upload

---
+++ SubClass2.java
@@ -0,0 +1,35 @@
+//Multilevel Inheritence – Example2
+
+
+
+package com.si.inheritance;
+
+class SuperClass
+{
+   public void methodA()
+   {
+     System.out.println("SuperClass");
+   }
+}
+class SubClass1 extends SuperClass
+{
+   public void methodB()
+   {
+     System.out.println("SubClass1 ");
+   }
+}
+
+class SubClass2 extends SubClass1
+{
+   public void methodC()
+   {
+     System.out.println("SubClass2");
+   }
+   public static void main(String args[])
+   {
+     SubClass2 obj = new SubClass2();
+     obj.methodA(); //calling super class method
+     obj.methodB(); //calling subclass 1 method
+     obj.methodC(); //calling own method
+  }
+}
