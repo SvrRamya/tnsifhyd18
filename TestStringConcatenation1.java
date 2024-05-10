c0de5fafd2cc18550f69b13dbac7726089e5b718
SvrRamya <168424860+SvrRamya@users.noreply.github.com>
2024-05-10 23:52:12 +0530

    Add files via upload

---
+++ TestStringConcatenation1.java
@@ -0,0 +1,12 @@
+//1) String Concatenation by + (String concatenation) operator
+
+
+package com.si.package1;
+
+public class TestStringConcatenation1 {
+	public static void main(String args[]){  
+		   String s="Sachin"+" Tendulkar";  
+		   System.out.println(s);//Sachin Tendulkar  
+		 }  
+
+}
