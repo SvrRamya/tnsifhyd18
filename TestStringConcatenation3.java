536f812ad4ba735ac3d23a8c77c4ce7913c93eaa
SvrRamya <168424860+SvrRamya@users.noreply.github.com>
2024-05-10 23:52:49 +0530

    Add files via upload

---
+++ TestStringConcatenation3.java
@@ -0,0 +1,15 @@
+//2) String Concatenation by concat() method
+
+
+package com.si.package1;
+
+public class TestStringConcatenation3 {
+	 public static void main(String args[]){  
+		   String s1="Sachin ";  
+		   String s2="Tendulkar";  
+		   String s3=s1.concat(s2);  
+		   System.out.println(s3);//Sachin Tendulkar  
+		  }  
+
+
+}
