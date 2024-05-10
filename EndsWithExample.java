8620fe1c7c26b3f8eb39f260f64793f7b1614f7a
SvrRamya <168424860+SvrRamya@users.noreply.github.com>
2024-05-10 23:42:12 +0530

    Add files via upload

---
+++ EndsWithExample.java
@@ -0,0 +1,13 @@
+//Java String endsWith() Method Example
+
+
+package com.si.package1;
+
+public class EndsWithExample {
+	public static void main(String args[]){  
+		String s1="java by javatpoint";  
+		System.out.println(s1.endsWith("t"));  
+		System.out.println(s1.endsWith("point"));  
+		}
+
+}
