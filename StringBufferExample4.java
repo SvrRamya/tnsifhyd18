01799f9f3b9397316d24e845e4e77bb6ad9dcb40
SvrRamya <168424860+SvrRamya@users.noreply.github.com>
2024-05-10 23:45:06 +0530

    Add files via upload

---
+++ StringBufferExample4.java
@@ -0,0 +1,13 @@
+//4) StringBuffer delete() Method
+
+
+package com.si.package1;
+
+public class StringBufferExample4 {
+	public static void main(String args[]){  
+		StringBuffer sb=new StringBuffer("Hello");  
+		sb.delete(1,3);  
+		System.out.println(sb);//prints Hlo  
+		}  
+
+}
