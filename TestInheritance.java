020f2a1a3a7d0761e19bb79f98c4a0faa61c053e
SvrRamya <168424860+SvrRamya@users.noreply.github.com>
2024-05-11 00:23:46 +0530

    Add files via upload

---
+++ TestInheritance.java
@@ -0,0 +1,30 @@
+//Java Single Inheritance Program Example –
+
+
+package com.si.inheritance;
+
+
+	class Animal
+	{  
+	   void eat()
+	   {
+	      System.out.println("eating...");
+	   }  
+	}  
+	class Dog extends Animal
+	{  
+	   void bark()
+	   {
+	      System.out.println("barking...");
+	   }  
+	}  
+
+	class TestInheritance
+	{  
+	   public static void main(String args[])
+	   {  
+	      Dog d=new Dog();  
+	      d.bark();  
+	      d.eat();  
+	   }
+	}
