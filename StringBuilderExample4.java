66604fc431c896b83eda54d92499f9ab36f4e6e0
SvrRamya <168424860+SvrRamya@users.noreply.github.com>
2024-05-10 23:48:06 +0530

    Add files via upload

---
+++ StringBuilderExample4.java
@@ -0,0 +1,13 @@
+//4) StringBuilder delete() method
+
+
+package com.si.package1;
+
+public class StringBuilderExample4 {
+	public static void main(String args[]){  
+		StringBuilder sb=new StringBuilder("Hello");  
+		sb.delete(1,3);  
+		System.out.println(sb);//prints Hlo  
+		}  
+
+}
