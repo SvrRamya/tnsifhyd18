8e0ae2cbae6f5c09571db2d266f7657b6fe9d91f
SvrRamya <168424860+SvrRamya@users.noreply.github.com>
2024-05-10 23:49:23 +0530

    Add files via upload

---
+++ Testimmutablestring1.java
@@ -0,0 +1,11 @@
+package com.si.package1;
+
+public class Testimmutablestring1 {
+	public static void main(String args[]){  
+		   String s="Sachin";  
+		   s=s.concat(" Tendulkar");  
+		   System.out.println(s);  
+		 }  
+
+
+}
