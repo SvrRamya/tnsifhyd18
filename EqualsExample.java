a01900a9adea88f83d225e441266333a3b7328d7
SvrRamya <168424860+SvrRamya@users.noreply.github.com>
2024-05-10 23:42:42 +0530

    Add files via upload

---
+++ EqualsExample.java
@@ -0,0 +1,17 @@
+//Java String equals() Method Example
+
+
+package com.si.package1;
+
+public class EqualsExample {
+	public static void main(String args[]){  
+		String s1="javatpoint";  
+		String s2="javatpoint";  
+		String s3="JAVATPOINT";  
+		String s4="python";  
+		System.out.println(s1.equals(s2));//true because content and case is same  
+		System.out.println(s1.equals(s3));//false because case is not same  
+		System.out.println(s1.equals(s4));//false because content is not same  
+		}
+
+}
